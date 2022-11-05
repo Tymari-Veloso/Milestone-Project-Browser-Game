abdf2c20a0d3c55493ed285952287915f4b54678
Tymari Veloso <tymariveloso@gmail.com>
2022-11-05 13:50:54 -0700

    fixed src 3

--- project-files/card-library.js
+++ project-files/card-library.js
@@ -1,14 +1,14 @@
 //construct cards and give them a unique ID
-let dog = new Unit("Dog", 2, 3,"card-images/barn.jpg")
-let bear = new Unit("Bear", 10, 10,"card-images/bear.jpg")
-let cat = new Unit("Cat", 3, 2,"card-images/cat.jpg")
-let lion = new Unit("Lion", 7, 7,"card-images/lion.jpg")
-let squirrel = new Unit("Squirrel", 3, 3,"card-images/squirrel.jpg")
-let fish = new Unit("Fish", 4, 1,"card-images/fish.jpg")
-let chicken = new Unit("Chicken", 2, 1,"card-images/chicken.jpg")
-let cow = new Unit("Cow", 3, 7,"card-images/cow.jpg")
-let pigeon = new Unit("Pigeon", 1, 1,"card-images/pigeon.jpg")
-let snake = new Unit("Snake", 3, 1,"card-images/snake.jpg")
+let dog = new Unit("Dog", 2, 3, card-images/barn.jpg)
+let bear = new Unit("Bear", 10, 10, card-images/bear.jpg)
+let cat = new Unit("Cat", 3, 2, card-images/cat.jpg)
+let lion = new Unit("Lion", 7, 7, card-images/lion.jpg)
+let squirrel = new Unit("Squirrel", 3, 3, card-images/squirrel.jpg)
+let fish = new Unit("Fish", 4, 1, card-images/fish.jpg)
+let chicken = new Unit("Chicken", 2, 1, card-images/chicken.jpg)
+let cow = new Unit("Cow", 3, 7, card-images/cow.jpg)
+let pigeon = new Unit("Pigeon", 1, 1, card-images/pigeon.jpg)
+let snake = new Unit("Snake", 3, 1, card-images/snake.jpg)
 let cardLibrary = [
     dog,
     bear,
